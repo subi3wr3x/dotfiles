86cb87478f442fba8240e972cc1149d84414e860
iBurley <iBurley@live.com>
2017-11-08 02:05:20 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@ sudo apt install xorg i3 firefox fonts-hack-ttf lxappearance pcmanfm
 mkdir ~/.config/
 mkdir ~/.config/i3/
 mkdir ~/.config/i3status/
-curl http://raw.githubusercontent.com/iBurley/dotfiles/master/.xinitrc > ~/.xinitrc
-curl http://raw.githubusercontent.com/iBurley/dotfiles/master/.Xresources > ~/.Xresources
-curl http://raw.githubusercontent.com/iBurley/dotfiles/master/.vimrc > ~/.vimrc
-curl http://raw.githubusercontent.com/iBurley/dotfiles/master/.config/i3/config > ~/.config/i3/config
-curl http://raw.githubusercontent.com/iBurley/dotfiles/master/.config/i3status/config > ~/.config/i3status/config
+wget http://raw.githubusercontent.com/iBurley/dotfiles/master/.xinitrc > ~/.xinitrc
+wget http://raw.githubusercontent.com/iBurley/dotfiles/master/.Xresources > ~/.Xresources
+wget http://raw.githubusercontent.com/iBurley/dotfiles/master/.vimrc > ~/.vimrc
+wget http://raw.githubusercontent.com/iBurley/dotfiles/master/.config/i3/config > ~/.config/i3/config
+wget http://raw.githubusercontent.com/iBurley/dotfiles/master/.config/i3status/config > ~/.config/i3status/config
