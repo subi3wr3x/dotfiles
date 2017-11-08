1b36ab57853dd6b76c8f1ee7d077fb76b863a620
iBurley <iBurley@live.com>
2017-11-08 01:56:50 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,10 @@
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
-
 sudo apt install xorg i3 firefox fonts-hack-ttf lxappearance pcmanfm 
-
 mkdir ~/.config/
-
 mkdir ~/.config/i3/
-
 mkdir ~/.config/i3status/
-
 curl http://raw.githubusercontent.com/iBurley/dotfiles/master/.xinitrc > ~/.xinitrc
-
 curl http://raw.githubusercontent.com/iBurley/dotfiles/master/.Xresources > ~/.Xresources
-
 curl http://raw.githubusercontent.com/iBurley/dotfiles/master/.vimrc > ~/.vimrc
-
 curl http://raw.githubusercontent.com/iBurley/dotfiles/master/.config/i3/config > ~/.config/i3/config
-
 curl http://raw.githubusercontent.com/iBurley/dotfiles/master/.config/i3status/config > ~/.config/i3status/config
