fc767ccc5f9ec6943a7b34c6bae35a63666e390f
iBurley <iBurley@live.com>
2017-11-07 23:53:13 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+mkdir ~/.config/i3/
+mkdir ~/.config/i3status/
+curl http://raw.githubusercontent.com/iBurley/dotfiles/master/.xinitrc > ~/.xinitrc
+curl http://raw.githubusercontent.com/iBurley/dotfiles/master/.Xresources > ~/.Xresources
+curl http://raw.githubusercontent.com/iBurley/dotfiles/master/.vimrc > ~/.vimrc
+curl http://raw.githubusercontent.com/iBurley/dotfiles/master/.config/i3/config > ~/.config/i3/config
+curl http://raw.githubusercontent.com/iBurley/dotfiles/master/.config/i3status/config > ~/.config/i3status/config
