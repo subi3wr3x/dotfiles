3956199140451621d996cb33ca7f5565f086fa90
iBurley <iBurley@live.com>
2018-10-31 01:48:52 -0400

    Update fedora_setup.sh

--- fedora_setup.sh
+++ fedora_setup.sh
@@ -5,6 +5,7 @@ sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-r
 sudo dnf -y install fedora-workstation-repositories
 sudo dnf -y config-manager --set-enabled rpmfusion-nonfree-nvidia-driver
 sudo dnf -y install xorg-x11-drv-nvidia akmod-nvidia && sudo dnf update -y
+sudo sed -i '/WaylandEnable=false/s/^#//' /etc/gdm/custom.conf
 
 # Fix font rendering:
 sudo dnf -y install freetype-freeworld
@@ -22,7 +23,7 @@ echo "alias fullupdate='sudo dnf update -y && sudo dnf autoremove -y && sudo dnf
 
 # Auto-mount storage drive:
 sudo mkdir /mnt/Storage/
-sudo bash -c 'echo -e "UUID=64a9314e-07d8-47dd-94a6-d1a55851b12d /mnt/Storage/ ext4 defaults 0 0" >> /etc/fstab'
+sudo bash -c 'echo -e "UUID=058D8AF8507F01D9 /mnt/Storage ntfs defaults 0 0" >> /etc/fstab'
 sudo mount -a
 
 # Symlink music from storage drive
@@ -31,12 +32,12 @@ ln -s /mnt/Storage/Music/ ~/Music/
 # Set SELinux to permissive mode:
 # sudo setenforce 0
 
+# Install/Remove software:
+sudo dnf -y install gnome-music gnome-tweak-tool transmission-gtk mpv youtube-dl compat-ffmpeg28
+sudo dnf -y remove gnome-maps gnome-photos rhythmbox gnome-weather cheese
+
 # Enable Flathub and install software:
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.discordapp.Discord -y
 flatpak install flathub com.spotify.Client -y
 flatpak install flathub com.valvesoftware.Steam -y
-
-# Install/Remove software:
-sudo dnf -y install gnome-music gnome-tweak-tool transmission-gtk mpv youtube-dl compat-ffmpeg28
-sudo dnf -y remove gnome-maps gnome-photos rhythmbox gnome-weather cheese
