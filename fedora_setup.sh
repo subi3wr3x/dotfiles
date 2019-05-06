01f4d3b30b2e9b6faa5cfca032c1cbe49d7873b7
iBurley <iBurley@live.com>
2019-05-05 23:54:42 -0400

    Update fedora_setup.sh

--- fedora_setup.sh
+++ fedora_setup.sh
@@ -6,6 +6,10 @@ sudo dnf -y install fedora-workstation-repositories
 sudo dnf -y config-manager --set-enabled rpmfusion-nonfree-nvidia-driver
 sudo dnf -y install xorg-x11-drv-nvidia akmod-nvidia && sudo dnf upgrade -y
 
+# Enable Cisco OpenH264 repo and install codec:
+sudo dnf config-manager --set-enabled fedora-cisco-openh264
+sudo dnf install gstreamer1-plugin-openh264 mozilla-openh264
+
 # Fix font rendering:
 sudo ln -s /usr/share/fontconfig/conf.avail/10-hinting-slight.conf /etc/fonts/conf.d/
 sudo ln -s /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/
@@ -30,7 +34,7 @@ ln -s /mnt/Storage/Music/ ~/Music/
 echo 'vm.swappiness = 10' | sudo tee -a /etc/sysctl.d/99-sysctl.conf
 
 # Install/Remove software:
-sudo dnf -y install gnome-music gnome-tweak-tool transmission-gtk mpv youtube-dl compat-ffmpeg28 xclip
+sudo dnf -y install gnome-music gnome-tweak-tool transmission-gtk mpv youtube-dl ffmpeg compat-ffmpeg28 xclip
 sudo dnf -y remove gnome-maps gnome-photos rhythmbox gnome-weather cheese gnome-clocks gnome-contacts gnome-documents totem
 
 # Enable Flathub and install software:
